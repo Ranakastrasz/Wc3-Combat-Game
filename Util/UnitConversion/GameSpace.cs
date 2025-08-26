bcf53a16a2a9cea2f4b8ed3627d535f24af93d19
Ranakastrasz <Ranakastrasz@gmail.com>
2025-08-26 18:29:41 -0400

    New UnitConverter Library added.

---
+++ Util/UnitConversion/GameSpace.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wc3_Combat_Game.Util.UnitConversion
+{
+    public class GameSpace
+    {
+        //public const float PHYSICS_SCALE = 1/64f;
+        //public const float GRID_SCALE = 1/32f;
+
+        enum Space
+        {
+            World,
+            Physics,
+            Grid
+        }
+
+
+        class SpaceConverterFloat: UnitConverter<Space, float>
+        {
+            static SpaceConverterFloat()
+            {
+                BaseUnit = Space.World;
+
+                // Correct Physics <-> World conversions
+                RegisterConversion(Space.Physics,
+                    w => w * 64f,
+                    p => p / 64f);
+
+                // Correct Grid <-> World conversions
+                RegisterConversion(Space.Grid,
+                    w => w * 32f,
+                    g => g / 32f);
+            }
+        }
+
+        /// <summary>
+        /// Represents a distance in WorldSpace.
+        /// </summary>
+        public readonly struct WorldFloat
+        {
+            public float Value { get; }
+
+            public WorldFloat(float value)
+            {
+                Value = value;
+            }
+
+            // Explicit conversion from WorldFloat to PhysicsFloat
+            public static explicit operator PhysicsFloat(WorldFloat w)
+            {
+                var converter = new SpaceConverterFloat();
+                var physicsValue = converter.Convert(w.Value, Space.World, Space.Physics);
+                return new PhysicsFloat(physicsValue);
+            }
+
+            public override string ToString() => $"{Value} W";
+        }
+        /// <summary>
+        /// Represents a distance in PhysicsSpace.
+        /// </summary>
+        public readonly struct PhysicsFloat
+        {
+            public float Value { get; }
+            public PhysicsFloat(float value)
+            {
+                Value = value;
+            }
+            // Explicit conversion from PhysicsFloat to WorldFloat
+            public static explicit operator WorldFloat(PhysicsFloat p)
+            {
+                var converter = new SpaceConverterFloat();
+                var worldValue = converter.Convert(p.Value, Space.Physics, Space.World);
+                return new WorldFloat(worldValue);
+            }
+            public override string ToString() => $"{Value} P";
+        }
+
+        /// <summary>
+        /// represents a distance in GridSpace.
+        /// </summary>
+        public readonly struct GridFloat
+        {
+            public float Value { get; }
+            public GridFloat(float value)
+            {
+                Value = value;
+            }
+            // Explicit conversion from GridFloat to WorldFloat
+            public static explicit operator WorldFloat(GridFloat g)
+            {
+                var converter = new SpaceConverterFloat();
+                var worldValue = converter.Convert(g.Value, Space.Grid, Space.World);
+                return new WorldFloat(worldValue);
+            }
+            public override string ToString() => $"{Value} G";
+        }
+
+    }
+}
---
+++ Util/UnitConversion/Temperature.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wc3_Combat_Game.Util.UnitConversion
+{
+    enum Temperature
+    {
+        Celcius,
+        Fahrenheit,
+        Kelvin
+    }
+
+    class TemperatureConverter: UnitConverter<Temperature, float>
+    {
+        static TemperatureConverter()
+        {
+            BaseUnit = Temperature.Celcius;
+
+            // Correct Fahrenheit <-> Celsius conversions
+            RegisterConversion(Temperature.Fahrenheit,
+                c => (c * 9f / 5f) + 32f,
+                f => (f - 32f) * 5f / 9f);
+
+            // Correct Kelvin <-> Celsius conversions
+            RegisterConversion(Temperature.Kelvin,
+                c => c + 273.15f,
+                k => k - 273.15f);
+        }
+    }
+
+    /// <summary>
+    /// Represents a temperature in Celsius.
+    /// </summary>
+    public readonly struct Celsius
+    {
+        public float Value { get; }
+
+        public Celsius(float value)
+        {
+            Value = value;
+        }
+
+        // Explicit conversion from Celsius to Fahrenheit
+        public static explicit operator Fahrenheit(Celsius c)
+        {
+            var converter = new TemperatureConverter();
+            var fahrenheitValue = converter.Convert(c.Value, Temperature.Celcius, Temperature.Fahrenheit);
+            return new Fahrenheit(fahrenheitValue);
+        }
+
+        public override string ToString() => $"{Value} C";
+    }
+
+    /// <summary>
+    /// Represents a temperature in Fahrenheit.
+    /// </summary>
+    public readonly struct Fahrenheit
+    {
+        public float Value { get; }
+
+        public Fahrenheit(float value)
+        {
+            Value = value;
+        }
+
+        // Explicit conversion from Fahrenheit to Celsius
+        public static explicit operator Celsius(Fahrenheit f)
+        {
+            var converter = new TemperatureConverter();
+            var celsiusValue = converter.Convert(f.Value, Temperature.Fahrenheit, Temperature.Celcius);
+            return new Celsius(celsiusValue);
+        }
+
+        public override string ToString() => $"{Value} F";
+    }
+}
