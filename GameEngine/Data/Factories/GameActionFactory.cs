ed68ae60f156b89c2fea3ef90fba9fb97ce7f77d
Ranakastrasz <Ranakastrasz@gmail.com>
2025-10-24 23:37:16 -0400

    Added projectile and other action factory entries, not used or tested yet.

--- GameEngine/Data/Factories/GameActionFactory.cs
+++ GameEngine/Data/Factories/GameActionFactory.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using Wc3_Combat_Game.Entities.Units.Buffs;
 using Wc3_Combat_Game.GameEngine.Actions;
 using Wc3_Combat_Game.GameEngine.Actions.Interface;
+using Wc3_Combat_Game.GameEngine.Data.Data;
 using Wc3_Combat_Game.Util.UnitConversion;
 
 namespace Wc3_Combat_Game.GameEngine.Data.Factories
@@ -31,6 +32,28 @@ namespace Wc3_Combat_Game.GameEngine.Data.Factories
         public static BuffAction CreateBuffAction(IBuffable.BuffType buffType, float factor, float duration)
         {
             return new BuffAction($"buff_action_{buffType}_{factor}_{duration}", buffType, factor, duration);
+        }
+
+        public static ProjectileAction CreateProjectileAction(string baseId, ProjectileData projectileData)
+        {
+            return new ProjectileAction($"{baseId}_{projectileData.ID}", projectileData);
+        }
+
+
+        // Dunno if I really need these, but eh, for now seems sensible.
+        public static BuffAction CreateRecoilDebuffAction(float recoilFactor, float recoilDuration)
+        {
+            return new BuffAction($"recoil_debuff_action_{recoilFactor}_{recoilDuration}", IBuffable.BuffType.Slow, recoilFactor, recoilDuration);
+        }
+
+        public static BuffAction CreateSnareDebuffAction(float snareFactor, float snareDuration)
+        {
+            return new BuffAction($"snare_debuff_action_{snareFactor}_{snareDuration}", IBuffable.BuffType.Slow, snareFactor, snareDuration);
+        }
+
+        public static BuffAction CreateSprintBuffAction(float speedFactor, float sprintDuration)
+        {
+            return new BuffAction($"sprint_buff_action_{speedFactor}_{sprintDuration}", IBuffable.BuffType.Speed, speedFactor, sprintDuration);
         }
 
     }
--- GameEngine/Data/Factories/ProjectileFactory.cs
+++ GameEngine/Data/Factories/ProjectileFactory.cs
@@ -9,16 +9,17 @@ using System.Threading.Tasks;
 using Wc3_Combat_Game.Core;
 using Wc3_Combat_Game.Core.Context;
 using Wc3_Combat_Game.Entities.Components.Interface;
+using Wc3_Combat_Game.GameEngine.Actions.Interface;
+using Wc3_Combat_Game.GameEngine.Data.Data;
 
 namespace Wc3_Combat_Game.GameEngine.Data.Factories
 {
     public static class ProjectileFactory
     {
+        public static ProjectileData CreateBasicProjectile (float radius, float speed, float lifespan, IGameplayAction? impactAction, int polygonCount, Color color)
+        {
+            return new ProjectileData($"basic_projectile_{radius}_{speed}_{lifespan}_{(impactAction != null ? impactAction.ID : "n/a")}_{polygonCount}_{color}", radius, speed, lifespan, impactAction, polygonCount, color);
+        }
 
-        //public static ProjectileFactory CreateBasicProjectile()
-        //{
-        //    var projectile = new ProjectilePrototype("Basic", 15f, 2f, 4f, 50f, Color.Brown, 6);
-        //    return projectile.AddImpactAction(AbilityFactory.CreateMeleeWeapon(5f));
-        //}
     }
 }
